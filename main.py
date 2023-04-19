de247bb8d88fa68fa98ada9a089cef70fe979558
millionstr <rsafonov@ukr.net>
2023-04-19 12:53:46 +0300

    initial progect

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def draw_house(x, y, width, height):
+    """
+    Mалюємо будинок висотою height, шириною width з координатами
+      нижньої опорної точки x,y
+    Args:
+        x (int): координата x
+        y (int)): кордината y
+        width (int): ширина будинку
+        height (int): висота будинку
+    """
+    print("малюємо будинок")
+    pass
+
+
+x, y = 100, 100
+width, height = 20, 40
+
+draw_house(x, y, width, height)
